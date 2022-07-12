ed18af66ea63633cb2d1fb28b227f22a2b568ee2
Pl1nxe <smirnyh.pavel@mail.ru>
2022-07-12 19:06:04 +0300

    Final commit.

--- practice-management-jmix-project/src/main/java/ru/haulmont/management/practice/screen/event/EventEdit.java
+++ practice-management-jmix-project/src/main/java/ru/haulmont/management/practice/screen/event/EventEdit.java
@@ -1,11 +1,32 @@
 package ru.haulmont.management.practice.screen.event;
 
+import io.jmix.core.Messages;
+import io.jmix.ui.Notifications;
 import io.jmix.ui.model.DataContext;
 import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.haulmont.management.practice.entity.Event;
 
 @UiController("Event.edit")
 @UiDescriptor("event-edit.xml")
 @EditedEntityContainer("eventDc")
 public class EventEdit extends StandardEditor<Event> {
+
+    @Autowired
+    protected Notifications notifications;
+
+    @Autowired
+    private Messages messages;
+
+    @Subscribe
+    public void onBeforeCommitChanges(BeforeCommitChangesEvent event) {
+        if (getEditedEntity().getStartDate().isAfter(getEditedEntity().getEndDate())) {
+            notifications.create()
+                    .withCaption(messages.getMessage("ru.haulmont.management.practice/exception.IncorrectTimeRangeExceptionHeader"))
+                    .withDescription(messages.getMessage("ru.haulmont.management.practice/exception.IncorrectTimeRangeException"))
+                    .withType(Notifications.NotificationType.WARNING)
+                    .show();
+            event.preventCommit();
+        }
+    }
 }
--- practice-management-jmix-project/src/main/java/ru/haulmont/management/practice/screen/student/StudentEdit.java
+++ practice-management-jmix-project/src/main/java/ru/haulmont/management/practice/screen/student/StudentEdit.java
@@ -1,10 +1,31 @@
 package ru.haulmont.management.practice.screen.student;
 
+import io.jmix.core.Messages;
+import io.jmix.ui.Notifications;
 import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.haulmont.management.practice.entity.Student;
 
 @UiController("Student.edit")
 @UiDescriptor("student-edit.xml")
 @EditedEntityContainer("studentDc")
 public class StudentEdit extends StandardEditor<Student> {
+
+    @Autowired
+    protected Notifications notifications;
+
+    @Autowired
+    private Messages messages;
+
+    @Subscribe
+    public void onBeforeCommitChanges(BeforeCommitChangesEvent event) {
+        if (getEditedEntity().getStartDate().isAfter(getEditedEntity().getEndDate())) {
+            notifications.create()
+                    .withCaption(messages.getMessage("ru.haulmont.management.practice/exception.IncorrectTimeRangeExceptionHeader"))
+                    .withDescription(messages.getMessage("ru.haulmont.management.practice/exception.IncorrectTimeRangeException"))
+                    .withType(Notifications.NotificationType.WARNING)
+                    .show();
+            event.preventCommit();
+        }
+    }
 }
